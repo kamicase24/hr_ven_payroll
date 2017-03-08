6f2bf6308d469bd5eddd4d88925399e4fb6e08a0
Jesus Rojas <kamicase24@hotmail.com>
2017-03-08 16:06:41 -0400

    nuevo manifest y gitignore

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,19 @@
     'name' : 'Module for Venelezuelan payroll',
     'version' : '1.0',
 	'author' : 'kamicase24',
-    'description' : """ Add fields in hr_payroll module for the Venezuelan payroll
+    'description' : """ 
+ Add custom fields in hr_payroll module for the Venezuelan payroll
  * Amount of Monday that owns a month
  * Month name and number
  * Final number day that owns a month
  * Extra hours
+ Calculate the state taxes and more
 """,
-    'sequence' : 39,
+    'sequence' : 99,
     'category' : 'Human Resources',
-    'website' : '',
+    'price' : 30.00,
+    'currency' : 'USD'
+    'website' : 'jrojas.odoo.com',
     'depends' : ['hr_payroll'],
     'data' : [
         'views/hr_payroll_views.xml',
